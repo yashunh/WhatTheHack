87cd8d4b96d1a291a95e9225414e682577c32320
yashunh <priyanshuntak@gmail.com>
2025-03-05 12:05:31 +0530

    logs

--- app/actions/contact.ts
+++ app/actions/contact.ts
@@ -10,11 +10,14 @@ export async function contact(props : {
     email: string,
     message: string
 }) {
+    console.log("inside contact")
     const result = emailSchema.safeParse(props.email)
     if(!result.success){
         console.log(result)
         return "invalid email"
     }
+    console.log("before query")
+
     const query = await client.query.create({
         data: {
             name: props.name,
